9ca19148f9039467b2d3b49fe42c97d41c97325c
Hyattria <xtmike008@qq.com>
2020-03-25 20:55:08 +0800

    feat: 从私库改到NPM

--- packages/modoo-script/utils/get-list.js
+++ packages/modoo-script/utils/get-list.js
@@ -4,7 +4,7 @@ module.exports = () => {
   let list = [];
   try {
     const listJSON = execSync(
-      "npm search  --json --registry http://47.116.3.37:4873/ @modoo/modoo-template"
+      "npm search  --json --registry https://registry.npmjs.com/ @modoo/modoo-template"
     );
     list = JSON.parse(listJSON);
   } catch (error) {}
